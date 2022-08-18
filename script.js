d99577e5f84e238a65e0bf3b238959cb4b5d0df0
Вова <initrest@gmail.com>
2022-08-18 20:26:32 +0300

    Todo2

--- script.js
+++ script.js
@@ -1,80 +1,112 @@
-let items = [];
+let taskId = 0;
+let tasks = [];
+let completedTasks = [];
 let form = document.querySelector('.popup__form');
-let inputTitle = document.querySelector('.popup__input');
-
-function renderTodoArray() {
-
-	let fullTemplate = '';
-
-	for (let index = 0; index < items.length; index++) {
-		let template = `
-            <div class="checkbox">
-                <input id="checkbox${items[index].title}" type="checkbox" class="checkbox__input">
-                <label for="checkbox${items[index].title}" class="checkbox__label">
-                    <div class="checkbox__text">
-                        <div class="checkbox__title">
-                            ${items[index].title}
-                        </div>
-                    </div>
-                </label>
-            </div>
-        `;
-		fullTemplate = fullTemplate + template;
-		document.querySelector('.todo__list').innerHTML = fullTemplate;
-	}
-}
-
-renderTodoArray();
-
-function addItem() {
-	items.push({
-		title: inputTitle.value
-	});
-
-	renderTodoArray();
-	popupBg.classList.remove('todo__overlay_active');
-	popup.classList.remove('popup_active');
-	document.querySelector('.popup__input').value = "";
-}
-
-form.addEventListener('submit', addItem);
-
-function removeItem(event) {
-	if (event.target.classList.contains('checkbox__input')) {
-		event.target.parentElement.remove();
-		items.splice(event.id, 1);
-	}
-}
-
-let todoElement = document.querySelector('.todo__list');
-todoElement.addEventListener('click', removeItem);
-
+let popupInputTitle = document.querySelector('.popup__input-title');
+let popupInputDescription = document.querySelector('.popup__input-description');
 let popupBg = document.querySelector('.todo__overlay');
 let popup = document.querySelector('.popup');
-let btn = document.querySelector('.todo__add');
+let incomplete = document.querySelector('#incomplete');
+let completed = document.querySelector('#completed');
+let incompleteTasksList = document.querySelector('.incomplete-Tasks-List');
+let completedTasksList = document.querySelector('.completed-Tasks-List');
+let contentSubtitle = document.querySelector('.content__subtitle');
+let addButton = document.querySelector('.todo__add');
+let incompleteList = document.querySelector('#incompleteList');
 
+function renderTasks() {									// Отрисовка уже определенных задач
+	tasks.forEach(task => {
+		const cloneTemplate = incomplete.content.cloneNode(true);
+		const titleElement = cloneTemplate.querySelector('.checkbox__title');
+		titleElement.textContent = task.title;
 
-function addpopup() {
-	popupBg.classList.add('todo__overlay_active');
-	popup.classList.add('popup_active');
+		const descriptionElement = cloneTemplate.querySelector('.checkbox__subtitle');
+		descriptionElement.textContent = task.description;
+		incompleteTasksList.append(cloneTemplate);
+	});
+
+}
+
+renderTasks();
+
+function openOrClosePopup() {
+	popupBg.classList.toggle('todo__overlay_active');
+	popup.classList.toggle('popup_active');
 }
 
 function closepopup(event) {
 	if (event.target.classList.contains('popup')) {
-		popupBg.classList.remove('todo__overlay_active');
-		popup.classList.remove('popup_active');
+		openOrClosePopup();
 	}
 }
 
 document.addEventListener('keydown', function (event) {
 	if (event.key === '+') {
-		addpopup();
+		openOrClosePopup();
 	}
 });
 
 popupBg.addEventListener('click', closepopup);
+addButton.addEventListener('click', openOrClosePopup);
 
-btn.addEventListener('click', addpopup);
+function addTask() {											// Добавление задачи
+	tasks.push({
+		title: popupInputTitle.value,
+		description: popupInputDescription.value,
+		id: taskId
+	});
+
+	const cloneTemplate = incomplete.content.cloneNode(true);
+	const titleElement = cloneTemplate.querySelector('.checkbox__title');
+	const inputElement = cloneTemplate.querySelector('.checkbox__input');
+	const labelElement = cloneTemplate.querySelector('.checkbox__label');
+	const descriptionElement = cloneTemplate.querySelector('.checkbox__subtitle');
+
+	inputElement.setAttribute('id', taskId);
+	labelElement.setAttribute('for', taskId);
+	titleElement.textContent = popupInputTitle.value;
+	descriptionElement.textContent = popupInputDescription.value;
+	incompleteTasksList.append(cloneTemplate);
+
+	openOrClosePopup();
+	document.querySelector('.popup__input-title').value = "";
+	document.querySelector('.popup__input-description').value = "";
+	taskId += 1;
+	contentSubtitle.textContent = `${tasks.length} incomplete, ${completedTasks.length} completed`;
+}
+
+form.addEventListener('submit', addTask);
+
+function removeItem(event) {									// Пометка о завершении задачи
+
+	if (event.target.classList.contains('checkbox__input')) {
+
+		let elementForDelete = tasks.find(item => item.id == event.target.id);
+
+		const cloneTemplate = completed.content.cloneNode(true);
+		const titleElement = cloneTemplate.querySelector('.checkbox__title');
+		const inputElement = cloneTemplate.querySelector('.checkbox__input');
+		const labelElement = cloneTemplate.querySelector('.checkbox__label');
+		const descriptionElement = cloneTemplate.querySelector('.checkbox__subtitle');
+
+		inputElement.setAttribute('id', elementForDelete.id);
+		labelElement.setAttribute('for', elementForDelete.id);
+		titleElement.textContent = elementForDelete.title;
+		descriptionElement.textContent = elementForDelete.description;
+		completedTasksList.prepend(cloneTemplate);
+		completedTasks.unshift(elementForDelete);
+		event.target.parentElement.remove();
+		tasks.splice(tasks.indexOf(elementForDelete), 1);
+		contentSubtitle.textContent = `${tasks.length} incomplete, ${completedTasks.length} completed`;
+	}
+}
+
+incompleteList.addEventListener('click', removeItem);
+
+
+
+
+
 
 
 
